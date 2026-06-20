92a8fcb5e81396e64fdeb74f131e03cb6ab828b4
Satyam <priyanshusinha1711@gmail.com>
2026-06-20 20:30:09 +0530

    Updated testcase to make every pass

--- test_app.py
+++ test_app.py
@@ -3,7 +3,7 @@ from app import add, divide
 
 class TestApp(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(2, 3), 4)
+        self.assertEqual(add(2, 3), 5)
 
     def test_divide(self):
         self.assertEqual(divide(10, 2), 5)
