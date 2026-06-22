dc95c409eba42d8864b88982a20b48945dc5d0a7
Satyam <priyanshusinha1711@gmail.com>
2026-06-22 21:14:10 +0530

    Multiple test case

---
+++ test_app_og.py
@@ -0,0 +1,16 @@
+import unittest
+from app import add, divide
+s
+class TestApp(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            divide(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()
